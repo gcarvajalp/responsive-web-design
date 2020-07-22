8cdcb4bdf7851f5da178cc952fe3dd035d363dbc
gabriel carvajal pizarro <g.carvajal.pizarro@gmail.com>
2020-07-22 19:16:11 -0400

    .

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -7,7 +7,7 @@ fi
 
 domains=(webcrapp.ddns.net)
 rsa_key_size=4096
-data_path="./data/certbot"
+data_path="./config/data"
 email="g.carvajal.pizarro@gmail.com" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
